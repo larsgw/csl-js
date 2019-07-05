cac9b1cdcfc2a1adc821b56428b750e42cf5fd71
Lars Willighagen <lars.willighagen@gmail.com>
2019-07-06 00:59:10 +0200

    feat: proper exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import '@babel/polyfill'
+import 'regenerator-runtime/runtime'
 import './logger'
 
 // xml parse: html-entities/lib/xml-entities
+
+import Formatter from './format'
+import locales from './locales'
+import styles from './styles'
+
+export { Formatter, locales, styles }
