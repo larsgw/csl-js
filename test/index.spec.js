7c2d89748dc2ebb28657befa43208f63bd1a2464
Lars Willighagen <lars.willighagen@gmail.com>
2019-08-09 23:12:53 +0200

    test: always load default locale for fallback

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,9 @@
 /* eslint-env mocha */
 
+require('isomorphic-fetch')
+
 const assert = require('assert')
-const { Formatter, styles } = require('../src/')
+const { Formatter, styles, locales } = require('../src/')
 
 const fs = require('fs')
 const path = require('path')
@@ -36,6 +38,10 @@ function format (formatter, mode, data) {
 const ROOT_PATH = path.join(__dirname, '../fixtures/processor-tests/humans')
 
 describe('fixtures', function () {
+  before('loading locale', async function() {
+    locales.add('en-US', await (await fetch('https://cdn.jsdelivr.net/gh/citation-style-language/locales@master/locales-en-US.xml')).text())
+  })
+
   for (let fixturePath of fs.readdirSync(ROOT_PATH)) {
     const fixtureName = path.basename(fixturePath, path.extname(fixturePath))
     const fixture = parse(fs.readFileSync(path.join(ROOT_PATH, fixturePath), 'utf8'))
